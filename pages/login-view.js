ac99942a08d36ad3a07033d4223841cc73c66fef
rishiparmr <therishiparmar@gmail.com>
2023-10-07 18:55:46 -0400

    fixed bugs with import statement paths

--- pages/login-view.js
+++ pages/login-view.js
@@ -16,11 +16,11 @@ import { useState } from "react";
 import LoginCard, {
   email,
   password,
-} from "../../components/login-card/login-card";
-import loginwaves from "../../assets/loginwaves.png";
+} from "../components/login-card/login-card";
+import loginwaves from "../assets/loginwaves.png";
 //import auth from "@react-native-firebase/auth";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { FIREBASE_AUTH } from "../../node_modules/firebaseConfig.ts";
+import { FIREBASE_AUTH } from "../firebaseConfig";
 
 const LoginView = () => {
   const [email, setEmail] = useState();
