c7d81dcd088f28faa87bb9ace320714d3ad5194f
rishiparmr <therishiparmar@gmail.com>
2023-10-07 16:16:39 -0400

    code-cleanup-nav

--- pages/login-view.js
+++ pages/login-view.js
@@ -1,252 +1,246 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    Pressable,
-    SafeAreaView,
-    ImageBackground,
-    TouchableWithoutFeedback,
-    Keyboard,
-    Alert,
-    StatusBar,
-    Platform,
-  } from "react-native";
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  SafeAreaView,
+  ImageBackground,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+  StatusBar,
+  Platform,
+} from "react-native";
+import LoginCard from "../components/login-card/login-card";
+import loginwaves from "../assets/loginwaves.png"
 
-  import { LinearGradient } from 'expo-linear-gradient';
-  import LoginCard from "../components/login-card/login-card";
-  import loginwaves from "../assets/loginwaves.png";
-  
-  const LoginView = () => {
-    const onPressFunction = () => {
-      Alert.alert("Login Pressed", "Thank you come again!", [
-        { text: "OK", onPress: () => console.log("OK Pressed") },
-      ]);
-    };
-    if (Platform.OS === "web") {
-      return (
-        <SafeAreaView style={styles.container}>
-          <View style={styles.loginView}>
-            <View
+const LoginView = ({ navigation }) => {
+  if (Platform.OS === "web") {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.loginView}>
+          <View
+            style={{
+              padding: 22,
+              // backgroundColor: "#f4f4ff",
+              borderRadius: 20,
+              // bottom: 0,
+              // marginTop: 10,
+            }}
+          >
+            <Text
               style={{
-                padding: 22,
-                // backgroundColor: "#f4f4ff",
-                borderRadius: 20,
-                // bottom: 0,
-                // marginTop: 10,
+                fontSize: 70,
+                fontWeight: 700,
+                color: Platform.OS === "web" ? "#000" : "#f4f4ff",
+                // shadowColor: "#171717",
+                // shadowOffset: { width: -1, height: 2 },
+                // shadowOpacity: 0.25,
+                // shadowRadius: 2,
+                textAlign: "center",
               }}
             >
-              <Text
-                style={{
-                  fontSize: 70,
-                  fontWeight: 700,
-                  color: Platform.OS === "web" ? "#000" : "#f4f4ff",
-                  // shadowColor: "#171717",
-                  // shadowOffset: { width: -1, height: 2 },
-                  // shadowOpacity: 0.25,
-                  // shadowRadius: 2,
-                  textAlign: "center",
-                }}
-              >
-                FairShare
-              </Text>
+              FairShare
+            </Text>
+          </View>
+
+          <View
+            style={{
+              gap: 20,
+              // width: "120%",
+              // height: "100%",
+              flexDirection: "column",
+              // justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <View style={{ gap: 10 }}>
+              <View>
+                <Text style={{ fontSize: 24, fontWeight: 700 }}>Login</Text>
+                <Text
+                  style={{
+                    fontSize: 15,
+                    color: "rgba(0,0,0,0.5)",
+                    fontWeight: 400,
+                  }}
+                >
+                  Please login to continue
+                </Text>
+              </View>
+
+              <LoginCard />
             </View>
-  
+
             <View
               style={{
                 gap: 20,
-                // width: "120%",
-                // height: "100%",
-                flexDirection: "column",
-                // justifyContent: "center",
                 alignItems: "center",
+
+                // marginTop: 10,
               }}
             >
-              <View style={{ gap: 10 }}>
-                <View>
-                  <Text style={{ fontSize: 24, fontWeight: 700 }}>Login</Text>
-                  <Text
-                    style={{
-                      fontSize: 15,
-                      color: "rgba(0,0,0,0.5)",
-                      fontWeight: 400,
-                    }}
-                  >
-                    Please login to continue
-                  </Text>
-                </View>
-  
-                <LoginCard />
-              </View>
-  
+              <Pressable
+                style={{
+                  padding: 15,
+                  backgroundColor: "#23B26E",
+                  borderRadius: 10,
+                  width: 250,
+                  shadowColor: "#171717",
+                  shadowOffset: { width: -1, height: 4 },
+                  shadowOpacity: 0.25,
+                  shadowRadius: 4,
+                }}
+                onPress={() => navigation.navigate("HomeScreen")}
+              >
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontWeight: 600,
+                    color: "#fff",
+                    textAlign: "center",
+                  }}
+                >
+                  Login
+                </Text>
+              </Pressable>
+              <Text style={{ textAlign: "center" }}>
+                Don't have an account?
+                <Text style={{ fontWeight: "bold" }}> Sign Up</Text>
+              </Text>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <ImageBackground
+        source={loginwaves}
+        resizeMode="contain"
+        style={styles.image}
+      >
+        <StatusBar hidden />
+        <SafeAreaView style={styles.container}>
+          <TouchableWithoutFeedback
+            onPress={Keyboard.dismiss}
+            accessible={false}
+          >
+            <View style={styles.loginView}>
               <View
                 style={{
-                  gap: 20,
-                  alignItems: "center",
-  
+                  padding: 22,
+                  // backgroundColor: "#f4f4ff",
+                  borderRadius: 20,
+                  // bottom: 0,
                   // marginTop: 10,
                 }}
               >
-                <Pressable
+                <Text
                   style={{
-                    padding: 15,
-                    backgroundColor: "#23B26E",
-                    borderRadius: 10,
-                    width: 250,
-                    shadowColor: "#171717",
-                    shadowOffset: { width: -1, height: 4 },
-                    shadowOpacity: 0.25,
-                    shadowRadius: 4,
+                    fontSize: 70,
+                    fontWeight: 700,
+                    color: Platform.OS === "web" ? "#000" : "#f4f4ff",
+                    // shadowColor: "#171717",
+                    // shadowOffset: { width: -1, height: 2 },
+                    // shadowOpacity: 0.25,
+                    // shadowRadius: 2,
+                    textAlign: "center",
                   }}
-                  onPress={() => onPressFunction()}
                 >
-                  <Text
-                    style={{
-                      fontSize: 20,
-                      fontWeight: 600,
-                      color: "#fff",
-                      textAlign: "center",
-                    }}
-                  >
-                    Login
-                  </Text>
-                </Pressable>
-                <Text style={{ textAlign: "center" }}>
-                  Don't have an account?
-                  <Text style={{ fontWeight: "bold" }}> Sign Up</Text>
+                  FairShare
                 </Text>
               </View>
-            </View>
-          </View>
-        </SafeAreaView>
-      );
-    } else {
-      return (
-        <ImageBackground
-          source={loginwaves}
-          resizeMode="contain"
-          style={styles.image}
-        >
-          <StatusBar hidden />
-          <SafeAreaView style={styles.container}>
-            <TouchableWithoutFeedback
-              onPress={Keyboard.dismiss}
-              accessible={false}
-            >
-              <View style={styles.loginView}>
-                <View
-                  style={{
-                    padding: 22,
-                    // backgroundColor: "#f4f4ff",
-                    borderRadius: 20,
-                    // bottom: 0,
-                    // marginTop: 10,
-                  }}
-                >
-                  <Text
-                    style={{
-                      fontSize: 70,
-                      fontWeight: 700,
-                      color: Platform.OS === "web" ? "#000" : "#f4f4ff",
-                      // shadowColor: "#171717",
-                      // shadowOffset: { width: -1, height: 2 },
-                      // shadowOpacity: 0.25,
-                      // shadowRadius: 2,
-                      textAlign: "center",
-                    }}
-                  >
-                    FairShare
-                  </Text>
+
+              <View
+                style={{
+                  gap: 20,
+                  // width: "120%",
+                  // height: "100%",
+                  flexDirection: "column",
+                  // justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <View style={{ gap: 10 }}>
+                  <View>
+                    <Text style={{ fontSize: 24, fontWeight: 700 }}>Login</Text>
+                    <Text
+                      style={{
+                        fontSize: 15,
+                        color: "rgba(0,0,0,0.5)",
+                        fontWeight: 400,
+                      }}
+                    >
+                      Please login to continue
+                    </Text>
+                  </View>
+
+                  <LoginCard />
                 </View>
-  
+
                 <View
                   style={{
                     gap: 20,
-                    // width: "120%",
-                    // height: "100%",
-                    flexDirection: "column",
-                    // justifyContent: "center",
                     alignItems: "center",
+
+                    // marginTop: 10,
                   }}
                 >
-                  <View style={{ gap: 10 }}>
-                    <View>
-                      <Text style={{ fontSize: 24, fontWeight: 700 }}>Login</Text>
-                      <Text
-                        style={{
-                          fontSize: 15,
-                          color: "rgba(0,0,0,0.5)",
-                          fontWeight: 400,
-                        }}
-                      >
-                        Please login to continue
-                      </Text>
-                    </View>
-  
-                    <LoginCard />
-                  </View>
-  
-                  <View
+                  <Pressable
                     style={{
-                      gap: 20,
-                      alignItems: "center",
-  
-                      // marginTop: 10,
+                      padding: 15,
+                      backgroundColor: "#23B26E",
+                      borderRadius: 10,
+                      width: 250,
+                      shadowColor: "#171717",
+                      shadowOffset: { width: -1, height: 4 },
+                      shadowOpacity: 0.25,
+                      shadowRadius: 4,
                     }}
+                    //onPress={() => navigation.navigate("HomeScreen")}
+                    onPress={() =>  navigation.navigate("HomeScreen")}
                   >
-                    <Pressable
+                    <Text
                       style={{
-                        padding: 15,
-                        backgroundColor: "#23B26E",
-                        borderRadius: 10,
-                        width: 250,
-                        shadowColor: "#171717",
-                        shadowOffset: { width: -1, height: 4 },
-                        shadowOpacity: 0.25,
-                        shadowRadius: 4,
+                        fontSize: 20,
+                        fontWeight: 600,
+                        color: "#fff",
+                        textAlign: "center",
                       }}
-                      onPress={() => onPressFunction()}
                     >
-                      <Text
-                        style={{
-                          fontSize: 20,
-                          fontWeight: 600,
-                          color: "#fff",
-                          textAlign: "center",
-                        }}
-                      >
-                        Login
-                      </Text>
-                    </Pressable>
-                    <Text style={{ textAlign: "center" }}>
-                      Don't have an account?
-                      <Text style={{ fontWeight: "bold" }}> Sign Up</Text>
+                      Login
                     </Text>
-                  </View>
+                  </Pressable>
+                  <Text style={{ textAlign: "center" }}>
+                    Don't have an account?
+                    <Text style={{ fontWeight: "bold" }}> Sign Up</Text>
+                  </Text>
                 </View>
               </View>
-            </TouchableWithoutFeedback>
-          </SafeAreaView>
-        </ImageBackground>
-      );
-    }
-  };
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: Platform.OS === "web" ? "#f4f4ff" : "transparent",
-    },
-    loginView: {
-      padding: 20,
-      gap: 80,
-      flexDirection: "column",
-      justifyContent: "space-around",
-    },
-    image: {
-      flex: 1,
-      justifyContent: "center",
-      // width: "100%",
-    },
-  });
-  
-  export default LoginView;
+            </View>
+          </TouchableWithoutFeedback>
+        </SafeAreaView>
+      </ImageBackground>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Platform.OS === "web" ? "#f4f4ff" : "transparent",
+  },
+  loginView: {
+    padding: 20,
+    gap: 80,
+    flexDirection: "column",
+    justifyContent: "space-around",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    // width: "100%",
+  },
+});
+
+export default LoginView;
