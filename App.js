729cee36b2401ac30bf28b26a6dade97a5572dd9
DarshanPatil1128 <darshan.patil1128@gmail.com>
2023-10-07 23:51:58 -0400

    created pick items page to extract from json

--- App.js
+++ App.js
@@ -1,3 +1,17 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  ScrollView,
+  StatusBar,
+  Image,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import BillCard from "./components/bill-card/bill-card";
+import waves from "./assets/waves.png";
 import NewBill from "./pages/newBill";
 import HomeScreen from "./pages/home-view";
 import LoginView from "./pages/login-view";
@@ -7,26 +21,29 @@ import SignUp from "./pages/sign-up";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouteName="Login"
-      >
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="NewBill" component={NewBill} />
-        <Stack.Screen name="Login" component={LoginView} />
-        <Stack.Screen name="CameraScan" component={CameraScan} />
-        <Stack.Screen name="PickItems" component={PickItems} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName="Login"
+        >
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="NewBill" component={NewBill} />
+          <Stack.Screen name="Login" component={LoginView} />
+          <Stack.Screen name="CameraScan" component={CameraScan} />
+          <Stack.Screen name="PickItems" component={PickItems} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
